72366e528d5a83e032cc9b363fed1d60d166794b
Nathan Van Heyst <57072440+nvanheyst@users.noreply.github.com>
2025-04-09 16:52:18 -0400

    Add files via upload

---
+++ ros1_bridge.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+
+from geometry_msgs.msg import TwistStamped, Twist
+import json
+from nav_msgs.msg import Odometry
+import rospy
+from sensor_msgs.msg import Imu
+import zmq
+
+ROBOT_NAME = "/a300_0002"
+ROS1_IP = "192.168.131.5"
+ROS2_IP = "192.168.131.1"
+
+ROS1_ODOM_OUT_TOPIC = "/ros2_odom_out"
+ROS1_ODOM_FILTERED_OUT_TOPIC = "/ros2_odom_filtered_out"
+ROS1_CMD_VEL_OUT_TOPIC = "/ros2_cmd_vel_out"
+ROS1_IMU_OUT_TOPIC = "/ros2_imu_data"
+
+CMD_VEL_IN_TOPIC = f"{ROBOT_NAME}/cmd_vel"
+
+
+def jsonOdomMsgToRos(obj):
+    msg = Odometry()
+    msg.header.stamp = rospy.Time(
+        obj['data']['header']['stamp']['sec'], obj['data']['header']['stamp']['nanosec'])
+    msg.header.frame_id = obj['data']['header']['frame_id']
+    msg.child_frame_id = obj['data']['child_frame_id']
+    msg.pose.pose.position.x = obj['data']['pose']['pose']['position']['x']
+    msg.pose.pose.position.y = obj['data']['pose']['pose']['position']['y']
+    msg.pose.pose.position.z = obj['data']['pose']['pose']['position']['z']
+    msg.pose.pose.orientation.x = obj['data']['pose']['pose']['orientation']['x']
+    msg.pose.pose.orientation.y = obj['data']['pose']['pose']['orientation']['y']
+    msg.pose.pose.orientation.z = obj['data']['pose']['pose']['orientation']['z']
+    msg.pose.pose.orientation.w = obj['data']['pose']['pose']['orientation']['w']
+    msg.twist.twist.linear.x = obj['data']['twist']['twist']['linear']['x']
+    msg.twist.twist.linear.y = obj['data']['twist']['twist']['linear']['y']
+    msg.twist.twist.linear.z = obj['data']['twist']['twist']['linear']['z']
+    msg.twist.twist.angular.x = obj['data']['twist']['twist']['angular']['x']
+    msg.twist.twist.angular.y = obj['data']['twist']['twist']['angular']['y']
+    msg.twist.twist.angular.z = obj['data']['twist']['twist']['angular']['z']
+    return msg
+
+
+def jsonImuMsgToRos(obj):
+    msg = Imu()
+    msg.header.stamp = rospy.Time(
+        obj['data']['header']['stamp']['sec'], obj['data']['header']['stamp']['nanosec'])
+    msg.header.frame_id = obj['data']['header']['frame_id']
+    msg.orientation.x = obj['data']['orientation']['x']
+    msg.orientation.y = obj['data']['orientation']['y']
+    msg.orientation.z = obj['data']['orientation']['z']
+    msg.orientation.w = obj['data']['orientation']['w']
+    msg.angular_velocity.x = obj['data']['angular_velocity']['x']
+    msg.angular_velocity.y = obj['data']['angular_velocity']['y']
+    msg.angular_velocity.z = obj['data']['angular_velocity']['z']
+    msg.linear_acceleration.x = obj['data']['linear_acceleration']['x']
+    msg.linear_acceleration.y = obj['data']['linear_acceleration']['y']
+    msg.linear_acceleration.z = obj['data']['linear_acceleration']['z']
+    return msg
+
+
+def jsonCmdVelStampedMsgToRos(obj):
+    msg = TwistStamped()
+    msg.header.stamp = rospy.Time(
+        obj['data']['header']['stamp']['sec'], obj['data']['header']['stamp']['nanosec'])
+    msg.header.frame_id = obj['data']['header']['frame_id']
+    msg.twist.linear.x = obj['data']['linear']['x']
+    msg.twist.linear.y = obj['data']['linear']['y']
+    msg.twist.linear.z = obj['data']['linear']['z']
+    msg.twist.angular.x = obj['data']['angular']['x']
+    msg.twist.angular.y = obj['data']['angular']['y']
+    msg.twist.angular.z = obj['data']['angular']['z']
+    return msg
+
+
+def cmdVelMsgToJson(msg, topic):
+    data = {
+        'topic': topic,
+        'data': {
+            'linear': {
+                'x': msg.linear.x,
+                'y': msg.linear.y,
+                'z': msg.linear.z
+            },
+            'angular': {
+                'x': msg.angular.x,
+                'y': msg.angular.y,
+                'z': msg.angular.z
+            }
+        }
+    }
+    return data
+
+
+class Ros1Bridge:
+    def __init__(self):
+        rospy.init_node('ros1_bridge')
+
+        # Initialize ZMQ context and sockets
+        self.context = zmq.Context()
+
+        # Subscriber socket for receiving from ROS2
+        self.zmq_sub = self.context.socket(zmq.SUB)
+        self.zmq_sub.connect(f"tcp://{ROS2_IP}:5555")
+        self.zmq_sub.setsockopt_string(zmq.SUBSCRIBE, "")
+
+        # Publisher socket for sending to ROS2
+        self.zmq_pub = self.context.socket(zmq.PUB)
+        self.zmq_pub.bind(f"tcp://{ROS1_IP}:5556")
+
+        # ROS1 publishers
+        self.odom_pub = rospy.Publisher(
+            ROS1_ODOM_OUT_TOPIC, Odometry, queue_size=10)
+        self.odom_filtered_pub = rospy.Publisher(
+            ROS1_ODOM_FILTERED_OUT_TOPIC, Odometry, queue_size=10)
+        self.cmd_vel_pub = rospy.Publisher(
+            ROS1_CMD_VEL_OUT_TOPIC, TwistStamped, queue_size=10)
+        self.imu_pub = rospy.Publisher(ROS1_IMU_OUT_TOPIC, Imu, queue_size=10)
+
+        # ROS1 subscribers
+        self.cmd_vel_sub = rospy.Subscriber(
+            CMD_VEL_IN_TOPIC, Twist, self.cmd_vel_callback)
+
+        rospy.loginfo('ROS1 bridge node initialized')
+
+    def cmd_vel_callback(self, msg):
+        data = cmdVelMsgToJson(msg, CMD_VEL_IN_TOPIC)
+        print(f"publishing string: {data}")
+        self.zmq_pub.send_string(json.dumps(data))
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            try:
+                message = self.zmq_sub.recv_string(flags=zmq.NOBLOCK)
+                data = json.loads(message)
+                if data['topic'] == f'{ROBOT_NAME}/platform/odom':
+                    msg = jsonOdomMsgToRos(data)
+                    self.odom_pub.publish(msg)
+
+                elif data['topic'] == f'{ROBOT_NAME}/platform/odom_filtered':
+                    msg = jsonOdomMsgToRos(data)
+                    self.odom_filtered_pub.publish(msg)
+
+                elif data['topic'] == f'{ROBOT_NAME}/sensors/imu_0/data':
+                    msg = jsonImuMsgToRos(data)
+                    self.imu_pub.publish(msg)
+
+                elif data['topic'] == f'{ROBOT_NAME}/cmd_vel':
+                    msg = jsonCmdVelStampedMsgToRos(data)
+                    self.cmd_vel_pub.publish(msg)
+
+            except zmq.Again:
+                rospy.sleep(0.001)
+                continue
+            except Exception as e:
+                rospy.logerr(f'Error processing ZMQ message: {str(e)}')
+                rospy.sleep(0.001)
+                continue
+
+
+def main():
+    bridge = Ros1Bridge()
+    try:
+        bridge.spin()
+    except rospy.ROSInterruptException:
+        pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros2_bridge.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+
+from geometry_msgs.msg import TwistStamped
+import json
+from nav_msgs.msg import Odometry
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Imu
+import zmq
+
+ROBOT_NAME = "/a300_0002"
+ROS1_IP = "192.168.131.5"
+ROS2_IP = "192.168.131.1"
+
+ROS2_ODOM_TOPIC = f"{ROBOT_NAME}/platform/odom"
+ROS2_ODOM_FILTERED_TOPIC = f"{ROBOT_NAME}/platform/odom_filtered"
+ROS2_IMU_TOPIC = f"{ROBOT_NAME}/sensors/imu_0/data"
+CMD_VEL_OUT_TOPIC = f"{ROBOT_NAME}/cmd_vel"
+
+
+def odomMsgToJson(msg, topic):
+    data = {
+        'topic': topic,
+        'data': {
+            'header': {
+                'stamp': {
+                    'sec': msg.header.stamp.sec,
+                    'nanosec': msg.header.stamp.nanosec
+                },
+                'frame_id': msg.header.frame_id
+            },
+            'child_frame_id': msg.child_frame_id,
+            'pose': {
+                'pose': {
+                    'position': {
+                        'x': msg.pose.pose.position.x,
+                        'y': msg.pose.pose.position.y,
+                        'z': msg.pose.pose.position.z
+                    },
+                    'orientation': {
+                        'x': msg.pose.pose.orientation.x,
+                        'y': msg.pose.pose.orientation.y,
+                        'z': msg.pose.pose.orientation.z,
+                        'w': msg.pose.pose.orientation.w
+                    }
+                }
+            },
+            'twist': {
+                'twist': {
+                    'linear': {
+                        'x': msg.twist.twist.linear.x,
+                        'y': msg.twist.twist.linear.y,
+                        'z': msg.twist.twist.linear.z
+                    },
+                    'angular': {
+                        'x': msg.twist.twist.angular.x,
+                        'y': msg.twist.twist.angular.y,
+                        'z': msg.twist.twist.angular.z
+                    }
+                }
+            }
+        }
+    }
+    return data
+
+
+def imuMsgToJson(msg, topic):
+    data = {
+        'topic': topic,
+        'data': {
+            'header': {
+                'stamp': {
+                    'sec': msg.header.stamp.sec,
+                    'nanosec': msg.header.stamp.nanosec
+                },
+                'frame_id': msg.header.frame_id
+            },
+            'orientation': {
+                'x': msg.orientation.x,
+                'y': msg.orientation.y,
+                'z': msg.orientation.z,
+                'w': msg.orientation.w
+            },
+            'angular_velocity': {
+                'x': msg.angular_velocity.x,
+                'y': msg.angular_velocity.y,
+                'z': msg.angular_velocity.z
+            },
+            'linear_acceleration': {
+                'x': msg.linear_acceleration.x,
+                'y': msg.linear_acceleration.y,
+                'z': msg.linear_acceleration.z
+            }
+        }
+    }
+    return data
+
+
+class Ros2Bridge(Node):
+    def __init__(self):
+        super().__init__('ros2_bridge')
+
+        self.test_context = zmq.Context()
+
+        # Publisher socket for sending data to ROS1
+        self.zmq_pub = self.test_context.socket(zmq.PUB)
+        self.zmq_pub.bind(f"tcp://{ROS2_IP}:5555")
+
+        # Subscriber socket for receiving commands from ROS1
+        self.zmq_sub = self.test_context.socket(zmq.SUB)
+        self.zmq_sub.connect(f"tcp://{ROS1_IP}:5556")
+        self.zmq_sub.setsockopt_string(zmq.SUBSCRIBE, "")
+
+        # ROS2 subscribers
+        self.odom_sub = self.create_subscription(
+            Odometry, ROS2_ODOM_TOPIC, self.odom_callback, 10)
+
+        self.odom_filtered_sub = self.create_subscription(
+            Odometry, ROS2_ODOM_FILTERED_TOPIC, self.odom_filtered_callback, 10)
+
+        self.imu_sub = self.create_subscription(
+            Imu, ROS2_IMU_TOPIC, self.imu_callback, 10)
+
+        # ROS2 publisher for received commands
+        self.cmd_vel_pub = self.create_publisher(
+            TwistStamped, CMD_VEL_OUT_TOPIC, 10)
+
+        self.create_timer(0.01, self.check_zmq_messages)
+
+        self.get_logger().info('ROS2 bridge node initialized')
+
+    def odom_callback(self, msg):
+        self.get_logger().info(
+            f'Republishing odom to ROS1 (x: {round(msg.pose.pose.position.x, 2)}, y: {round(msg.pose.pose.position.y, 2)})')
+        data = odomMsgToJson(msg, ROS2_ODOM_TOPIC)
+        self.zmq_pub.send_string(json.dumps(data))
+
+    def imu_callback(self, msg):
+        data = imuMsgToJson(msg, ROS2_IMU_TOPIC)
+        self.zmq_pub.send_string(json.dumps(data))
+
+    def odom_filtered_callback(self, msg):
+        data = odomMsgToJson(msg, ROS2_ODOM_FILTERED_TOPIC)
+        self.zmq_pub.send_string(json.dumps(data))
+
+    def check_zmq_messages(self):
+        try:
+            message = self.zmq_sub.recv_string(flags=zmq.NOBLOCK)
+            data = json.loads(message)
+            print(data)
+            if data['topic'] == CMD_VEL_OUT_TOPIC:
+                msg = TwistStamped()
+                msg.header.stamp = self.get_clock().now().to_msg()
+                msg.twist.linear.x = data['data']['linear']['x']
+                msg.twist.linear.y = data['data']['linear']['y']
+                msg.twist.linear.z = data['data']['linear']['z']
+                msg.twist.angular.x = data['data']['angular']['x']
+                msg.twist.angular.y = data['data']['angular']['y']
+                msg.twist.angular.z = data['data']['angular']['z']
+                self.cmd_vel_pub.publish(msg)
+
+        except zmq.Again:
+            pass
+        except Exception as e:
+            self.get_logger().error(f'Error processing ZMQ message: {str(e)}')
+
+
+def main():
+    rclpy.init()
+    bridge = Ros2Bridge()
+
+    try:
+        rclpy.spin(bridge)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        bridge.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
